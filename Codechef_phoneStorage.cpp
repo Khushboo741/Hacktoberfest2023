2fd1470ad72c50ec5bdb6fa3b6a6fcb2bdc6c8ba
Khushboo741 <116384371+Khushboo741@users.noreply.github.com>
2023-10-24 16:27:22 +0530

    Create Codechef_phoneStorage.cpp

---
+++ Codechef_phoneStorage.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+      int t;
+      cin>>t;
+      while(t--){
+          int S,X,Y,Z;
+          cin>>S>>X>>Y>>Z;
+          if(S-X-Y>=Z){
+              cout<<0<<endl;
+          }
+          else if(S-X>=Z || S-Y>=Z){
+              cout<<1<<endl;
+          }
+          else{
+              cout<<2<<endl;
+          }
+      }
+	return 0;
+}
